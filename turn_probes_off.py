a6b2ab4d934366f55361d4b8f33ff3f8152a091f
Aled Jones <aledjones@nhs.net>
2017-11-01 18:03:22 +0000

    Update with comments and re-factored

--- turn_probes_off.py
+++ turn_probes_off.py
@@ -1,148 +1,157 @@
 import xml.etree.ElementTree as ET
-from bokeh.server.start import stop
 
-class edit_design_file():
-    def __init__(self):
-        # define input array xml files
-        self.old_array_design="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\array_design_028469\\028469_Guys19_110601.xml"
-        self.new_array_design="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\array_design_085030\\CGH 4.3_085030_D_F_20170809_Guys19.xml"
-        
-        # new bedfile
-        self.new_array_design_bedfile="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\array_design_085030\\CGH 4.3_085030_D_BED_20170809.bed"
-        
-        # set output files
-        self.filtered_array_design="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\CGH 4.3_085030_D_F_20170809_Guys19_FILTERED.xml"
-        self.filtered_array_design_bedfile="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\CGH 4.3_085030_D_BED_20170809_FILTERED.bed"
-        
-        # add to the array design name
-        self.name_to_modify_design_name="_filtered"
-        
-    def get_probe_list(self,xml_input_file):
-        '''this function takes a single array design and returns a list of probe names'''
-        # empty list to hold all probes in this design
-        probe_list=[]
-   
-        #load the xml file into python object
-        tree=ET.parse(xml_input_file)
-        # set the root of the file
-        root=tree.getroot()
-        
-        #loop through the file, looking at each 'reporter' section (each reporter section is a probe)
-        for probe in root.iter('reporter'):
-            # add the probe name to the list
-            probe_list.append(probe.attrib['name'])
-        #return list
-        return probe_list
+class FilterArrayDesign():
+    """
+    This class takes two array designs, an "old" design and a "new" design. The new design may not yet be ISO accredited so only the accredited probes can be used.
+    Array designs are in the form of xml files, where each probe are wrapped in a 'reporter' tag. There are different types of probes, including control probes which can be excluded from any analysis.
+    This script takes all the probes from the accredited old design and uses this to turn any probes on the new design into control probes, in effect turning the probes off.
+    The output is a "filtered" version of the "new" array design. 
     
-    def rewrite_control_file(self, oldprobelist, file_to_filter):
-        '''this function takes the lists of probes that are to be 'turned off from the new array design and rewrites the xml file'''
+    The script also creates a modified bedfile for the "new" design, removing any probes from the "new" design bed file which don't appear on the "old" array design 
+    """
+    
+    def __init__(self):
+        """Specify the input and output files"""
+        # input xml file for "old" design
+        self.OldArrayDesignXML="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\028469_Guys19_110601_truncated.xml"
+        # input xml file for "new" design
+        self.NewArrayDesignXML="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\CGH 4.3_085030_D_F_20170809_Guys19Truncated.xml"
         
-        #load the xml file into python object
-        tree=ET.parse(file_to_filter)
+        # input bedfile for "new" design
+        self.NewArrayDesignBedfile="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\array_design_085030\\CGH 4.3_085030_D_BED_20170809.bed"
         
-        # set the root of the file
-        root=tree.getroot()
+        # output xml file for "filtered" design
+        self.FilteredArrayDesignXML="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\truncatedtest.xml"
+        # output bedfile for "filtered" design
+        self.FilteredArrayDesignBedfile="S:\\Genetics_Data2\\Array\\Audits and Projects\\160421 ArrayCGH v4.0 Design\\modified_design_file\\dev\\ignore_me.bed"
+        
+                
+    def get_probe_list(self,XMLInputFile):
+        """this function takes a single array design and returns a list of probe names"""
+        # an empty list to hold all probes that will be identified when parsing the input design file
+        ProbeList=[]
+   
+        # Using the Element tree module parse the input xml file into python object
+        Tree=ET.parse(XMLInputFile)
+        
+        # set the root of the xml tree so we can use this to loop
+        Root=Tree.getroot()
         
         #loop through the file, looking at each 'reporter' section (each reporter section is a probe)
-        for probe in root.iter('reporter'):
+        for Probe in Root.iter('reporter'):
+            # add the probe name to the list
+            ProbeList.append(Probe.attrib['name'])
+        
+        print "get_probe_list"
+        
+        #return list of probes
+        return ProbeList
+    
+    def add_controltype_to_new_probes(self, OldProbeList, FileToFilter):
+        """
+        This function recieves the list of probes on the 'old' array design and the file path to the 'new' array design
+        The 'new' array XML design file is read into a python object and each probe is assessed. If the probe is not present in the 'old' array design it has an attribute of 
+        control_type = ignore added to 'turn off' the probe
+        The modified XML is then written to the 'filtered' array design file however this does not include non-XML data in the file such as the file type and header   
+        """
+        
+        # load the xml file input to this function into a python object using the element tree package
+        Tree=ET.parse(FileToFilter)
+        
+        # set the root of the file
+        Root=Tree.getroot()
+        
+        # loop through the file, looking at each 'reporter' section (each reporter section is a probe)
+        for Probe in Root.iter('reporter'):
             #if the probe name is not in the old array design
-            if probe.attrib['name'] not in oldprobelist:
+            if Probe.attrib['name'] not in OldProbeList:
                 # add a flag to ignore the probe
-                probe.attrib['control_type']="ignore"       
+                Probe.attrib['control_type']="ignore"       
                 
-        #write the new modified design to file 
-        tree.write(self.filtered_array_design)
+        # write the modified XML data object to the filtered XML output file.
+        Tree.write(self.FilteredArrayDesignXML)
         
-        #### Also need to write in some non-xml lines (comments and doc type)
-        
-        # open the filtered xml file created above 
-        with open(self.filtered_array_design,'r') as filtered_no_header_file:
-            
+                
+    def add_non_XML_to_XML_file(self):
+        """
+        Some non-XML lines are required to be added to the 'filtered' file in order for the design file to be recognised by the scanner.
+        These are found at the top of the XML file, before the project tag (usually around 10 lines).
+        The top 50 lines of the 'New' array design XML file are read into a list
+        The 'filtered' XML file is read back into a list and any lines before the project tag added into this list
+        This list is then written back to file
+        """
+        # open the filtered XML file created above 
+        with open(self.FilteredArrayDesignXML,'r') as FilteredXMLFileNoHeader:
             # create a list of all the lines in this file so we can insert lines in as required
-            filtered_no_header_list=filtered_no_header_file.readlines()
+            FilteredXMLFileNoHeaderList=FilteredXMLFileNoHeader.readlines()
         
         # open the original array design file to get the non-xml lines 
-        with open(self.new_array_design,'r') as new_array_design_file:
-            # capture the top 50 lines into a list
-            top_50_lines=[next(new_array_design_file) for x in xrange(50)]
+        with open(self.NewArrayDesignXML,'r') as NewArrayDesignFile:
+            # capture the top 50 lines into a list (this should include all the non-XML lines)
+            Top50Lines=[next(NewArrayDesignFile) for x in xrange(50)]
         
-        # the top 50 lines of both files should be the same (excluding any flags to ignore the probes)
-        # we only want the non-xml stuff which occurs before the first xml tag which is 'project'
-        # loop through each item in the enumerated list of top 50 lines
-        for number,line in enumerate(top_50_lines):
+        
+        # capture the first line of the 'filtered' XML file. This can be used to only bring in lines from the 'new' XML design before the XML which has already been captured 
+        # take the start of the line (<project) only as the order of the other tags can change
+        WhereToStop=str(FilteredXMLFileNoHeaderList[0][0:8])
+        
+        # loop through the list of lines from the 'new' array design. use enumerate so we can insert the lines into the list at the specific line 
+        for Number,Line in enumerate(Top50Lines):
             # set a flag so once we have reached the project tag we can stop
-            stop=False
-            # when we reach the project tag, change the stop variable to true
-            if line.startswith("<project"):
-                stop=True
+            StopFlag=False
+            # when we reach the project tag, change the StopFlag variable to true
+            if WhereToStop in Line:
+                # set the tag to true so
+                StopFlag=True
             # if we haven't reached the xml yet, add the non-xml line into the same place in the filtered xml file list
-            elif not stop:
-                filtered_no_header_list.insert(number,line)
-            # if we have already reached the xml pass over
+            elif not StopFlag:
+                FilteredXMLFileNoHeaderList.insert(Number,Line)
+            # if we have already reached the xml pass
             else:
                 pass
         
         # re-write the list with the non-xml added in to file.
-        with open(self.filtered_array_design,'w') as filtered_file_with_header:
-            filtered_file_with_header.writelines(filtered_no_header_list)
+        with open(self.FilteredArrayDesignXML,'w') as filtered_file_with_header:
+            filtered_file_with_header.writelines(FilteredXMLFileNoHeaderList)
     
-    def create_new_bedfile(self, old_probes):
-        '''This function takes the list of probes on the old array design as an input. It then opens the array_design bedfile in a list, loops through it assessing if the probe name is on the old array design. if it is the line is written to a new bedfile.'''
+    def create_new_bedfile(self, OldProbes):
+        """
+        This function takes the list of probes on the 'old' array design as an input. 
+        It then opens the 'new' array_design bedfile in a list, loops through it assessing if the probe name is on the 'old' array design. 
+        If it is the line is written to the 'filtered' bedfile.
+        """
         # open the new array design bedfile
-        with open(self.new_array_design_bedfile,'r') as inputbedfile:
+        with open(self.NewArrayDesignBedfile,'r') as InputBedfile:
             # put into a list
-            bedfile_list=inputbedfile.readlines()
-        # open a new file to write to
-        with open(self.filtered_array_design_bedfile,'w') as filtered_bed:
+            BedfileList=InputBedfile.readlines()
+        
+        # open a new file to write 'filtered' bed file
+        with open(self.FilteredArrayDesignBedfile,'w') as FilteredBed:
             # for line in bed file
-            for line in bedfile_list:
+            for Line in BedfileList:
                 # split the bedfile on tab, take the 4th column and remove any new line characters
-                probename=line.split('\t')[3].rstrip()
+                ProbeName=Line.split('\t')[3].rstrip()
                 # if the probe name is on the old array write to the new bedfile
-                if probename in set(old_probes):
-                    filtered_bed.write(line)
+                if ProbeName in set(OldProbes):
+                    FilteredBed.write(Line)
             
-    
-               
-        
-           
-    def what_to_do(self):
-        '''This function uses the above modules to create the new array file'''
-        # get lists of probes from new and old array designs
-        old_probelist=self.get_probe_list(self.old_array_design) 
-        #new_probelist=self.get_probe_list(self.new_array_design)
-        
-        # call function to write a design file where new probes have been filtered
-        self.rewrite_control_file(old_probelist, self.new_array_design)
-        
-        ########################################################################
-        # # count the number of probes to be excluded
-        # in_both_count=0
-        # new_probe=0
-        # for probe in new_probelist:
-        #     if probe in set(old_probelist):
-        #         in_both_count+=1
-        #     else:
-        #         new_probe+=1
-        # 
-        ########################################################################
-        
-        #create the bed file 
-        self.create_new_bedfile(old_probelist)
-        
-        ########################################################################
-        # #print some probe counts etc
-        # #print "probes on new design" + str(len(new_probelist))
-        # #print "probes on old design" + str(len(old_probelist))
-        # print "probes on both = "+str(in_both_count)
-        # print "new probes to be excluded = "+str(new_probe)
-        # #assert new_probelist==in_both_count+new_probe
-        # print self.to_be_ignored[0:50]
-        ########################################################################
+
+def main():
+    """
+    This function uses the filterarrayDesign class
+    """
+    #create instance of class
+    FilterArray=FilterArrayDesign()
+    # Create a list for all probes on the old array design
+    OldProbelist=FilterArray.get_probe_list(FilterArray.OldArrayDesignXML) 
+    # Create a file containing the probes which have been 'turned off'
+    FilterArray.add_controltype_to_new_probes(OldProbelist, FilterArray.NewArrayDesignXML)
+    # add in any non-XML lines to the 'filtered' XML file
+    FilterArray.add_non_XML_to_XML_file()
+    # create the 'filtered' bed file 
+    FilterArray.create_new_bedfile(OldProbelist)
+
+if __name__ =="__main__":
+    main()
 
         
-        
-#create instance of class        
-go=edit_design_file()
-#call function
-go.what_to_do()
